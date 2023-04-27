210cc1effe7412724be7da515554e399ff881532
Cristi Constantin <git@cristi.email>
2023-04-27 23:26:13 +0100

    Added JS files that work

--- js/index.js
+++ js/index.js
@@ -1,7 +1,54 @@
 import '../style.css';
 import { render, h, Component } from 'preact';
+import { throttle } from 'throttle-debounce';
+
+let memory = null;
+let consoleLogBuffer = '';
+const txtDecoder = new TextDecoder();
+
+const importObject = {
+  env: {
+    consoleLog: function (ptr, len) {
+      consoleLogBuffer += txtDecoder.decode(new Uint8Array(memory.buffer, ptr, len));
+    },
+    consoleFlush: function () {
+      console.log(consoleLogBuffer);
+      consoleLogBuffer = '';
+    },
+  },
+};
 
 (async function main() {
+  const module = await WebAssembly.compileStreaming(fetch('main.wasm'));
+  const { exports } = await WebAssembly.instantiate(module, importObject);
+  const { turn } = exports;
+  memory = exports.memory;
+
+  const mapWidth = new Uint8Array(memory.buffer, exports.mapWidth.value, 1)[0];
+  const mapHeight = new Uint8Array(memory.buffer, exports.mapHeight.value, 1)[0];
+  console.log(`Map size: ${mapWidth}x${mapHeight}`);
+
+  const viewWidth = new Uint8Array(memory.buffer, exports.viewWidth.value, 1)[0];
+  const viewHeight = new Uint8Array(memory.buffer, exports.viewHeight.value, 1)[0];
+  console.log(`View size: ${viewWidth}x${viewWidth}`);
+
+  // seed for random maps and creatures (U32)
+  const seed = Math.round(new Date() * Math.random() / 1000);
+  console.log('Random seed:', seed);
+  // important! init the game!
+  exports.init(seed);
+
+  // game map grid
+  let bufferOffset = exports.getMapPointer();
+  const areaTiles = new Uint16Array(memory.buffer, bufferOffset, mapWidth * mapHeight);
+
+  // flat rendered grid
+  bufferOffset = exports.getViewPointer();
+  const flatTiles = new Uint16Array(memory.buffer, bufferOffset, viewWidth * viewHeight);
+  bufferOffset = null;
+
+  let wasmMemorySz = memory.buffer.byteLength / (1024 * 1024);
+  console.log('WASM memory MB:', wasmMemorySz);
 
   class GameGrid extends Component {
     state = { turns: 0 };
@@ -12,14 +59,14 @@ import { render, h, Component } from 'preact';
         let ok = true;
         if (ev.key === 'w') {
           // try to move Nord, up
-          // ok = turn(0);
+          ok = turn(0);
         } else if (ev.key === 's') {
           // try to move South, down
-          // ok = turn(1);
+          ok = turn(1);
         } else if (ev.key === 'd') {
-          // ok = turn(2);
+          ok = turn(2);
         } else if (ev.key === 'a') {
-          // ok = turn(3);
+          ok = turn(3);
         } else {
           ok = false;
         }
@@ -46,8 +93,21 @@ import { render, h, Component } from 'preact';
 
     render() {
       const grid = [];
+      for (let r = 0; r < viewHeight; r++) {
+        const row = [];
+        for (let c = 0; c < viewWidth; c++) {
+          const i = c + r * viewWidth;
+          row.push(flatTiles[i]);
+        }
+        grid.push(row);
+      }
+
       const gridCol = (nr, c, r) => {
-        let ch = '.';
+        let ch = String.fromCharCode(nr);
+        if (!ch.trim()) ch = "'";
+        else if (ch === 'B') ch = '🦋';
+        else if (ch === 'X') ch = '▣';
+        else if (ch === 'x') ch = '▨';
         return h(
           'td',
           {
