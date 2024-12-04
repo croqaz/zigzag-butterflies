6c220da8a9c0ade209805db62e554b51aad6dd60
Cristi Constantin <git@cristi.email>
2024-12-04 15:40:32 +0000

    Save old changes

--- js/event.js
+++ js/event.js
@@ -1,2 +1,6 @@
-import Nanobus from 'nanobus';
-export const bus = Nanobus();
+import { createNanoEvents } from 'nanoevents';
+export const bus = createNanoEvents();
+bus.off = function (event, cb) {
+  if (cb) this.events[event] = this.events[event]?.filter((i) => cb !== i);
+  else this.events[event] = [];
+};
